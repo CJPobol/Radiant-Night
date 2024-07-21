a2b8fa491ea44ec1115ab8d35d7d5b1dd8294f75
CJPobol <cjp25@njit.edu>
2024-07-21 16:07:31 -0400

    Dialogue Skipping is functional!

--- Radiant Night/Assets/Scripts/DialogueManager.cs
+++ Radiant Night/Assets/Scripts/DialogueManager.cs
@@ -5,11 +5,16 @@ using TMPro;
 using Ink.Runtime;
 using Unity.PlasticSCM.Editor.WebApi;
 using System;
+using Unity.VisualScripting;
 
 [System.Serializable]
 public class DialogueManager : MonoBehaviour
 {
     private static DialogueManager instance;
+
+    [Header("Params")]
+    [SerializeField] private float textSpeed;
+    private Coroutine displayLineCoroutine;
 
     [Header("Dialogue UI")]
     [SerializeField] private GameObject dialoguePanel;
@@ -22,6 +27,9 @@ public class DialogueManager : MonoBehaviour
     private Story currentStory;
 
     public bool dialogueIsPlaying { get; private set; }
+    private bool canGoNextLine;
+    private bool skipDialogue;
+    private bool canSkip;
 
     private void Awake()
     {
@@ -52,11 +60,25 @@ public class DialogueManager : MonoBehaviour
     {
         if (!dialogueIsPlaying) return;
 
-        if (Input.GetMouseButtonDown(1)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            ContinueStory();
+            if (canGoNextLine)
+            {
+                ContinueStory();
+            }
+            else
+            {
+                skipDialogue = true;
+            }
         }
         
+    }
+
+    private IEnumerator CanSkip()
+    {
+        canSkip = false; //Making sure the variable is false.
+        yield return new WaitForSeconds(0.05f);
+        canSkip = true;
     }
 
     public static DialogueManager GetInstance()
@@ -77,11 +99,11 @@ public class DialogueManager : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
+            if (displayLineCoroutine != null) StopCoroutine(displayLineCoroutine);
+            
             //display dialogue line
-            dialogueTextDisplay.text = currentStory.Continue();
+            displayLineCoroutine = StartCoroutine(DisplayLine(currentStory.Continue()));
 
-            //display choices if applicable
-            DisplayChoices();
         }
         else
         {
@@ -89,18 +111,60 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
+    private IEnumerator DisplayLine(string line)
+    {
+        //empty dialogue text
+        dialogueTextDisplay.text = "";
+
+        canGoNextLine = false;
+        HideChoices();
+
+        StartCoroutine(CanSkip());
+        
+        foreach(char c in line.ToCharArray())
+        {
+            if (skipDialogue && canSkip)
+            {
+                dialogueTextDisplay.text = line;
+                //Debug.Log("Dialogue Skipped: " + line);
+                break;
+            }
+            dialogueTextDisplay.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        canGoNextLine = true;
+        skipDialogue = false;
+        canSkip = false;
+
+        //display choices if applicable
+        DisplayChoices();
+        
+    }
+
+    private void HideChoices()
+    {
+        foreach (GameObject choice in choices)
+        {
+            choice.SetActive(false);
+        }
+    }
+
     private void ExitDialogueMode()
     {
         dialogueIsPlaying = false;
         dialoguePanel.SetActive(false);
-        dialogueTextDisplay.text = "";
     }
 
     private void DisplayChoices()
     {
         List<Choice> currentChoices = currentStory.currentChoices;
 
-        if (currentChoices.Count > choices.Length)
+        //if there's a choice to be made, the player can't continue until they pick something
+        if (currentChoices.Count > 0)
+            canGoNextLine = false;
+
+            if (currentChoices.Count > choices.Length)
         {
             Debug.LogError("More choices than UI can currently support. Choice count: " + currentChoices.Count);
         }
@@ -124,6 +188,7 @@ public class DialogueManager : MonoBehaviour
 
     public void MakeChoice(int choiceIndex)
     {
+        skipDialogue = false;
         currentStory.ChooseChoiceIndex(choiceIndex);
         ContinueStory();
     }
