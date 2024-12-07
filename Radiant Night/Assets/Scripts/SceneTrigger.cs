41fdd4cc33142d80e15dfb65dd9d2a9673516f4f
CJPobol <cjp25@njit.edu>
2024-12-07 16:36:43 -0500

    can teleport character to new locations

---
+++ Radiant Night/Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public bool locationTrigger;
+    public Vector2 location;
+
+    public bool sceneTrigger;
+    public string sceneName;
+
+    public bool forceTrigger;
+
+    public GameObject player;
+    public GameObject visualCue;
+
+    private bool playerInRange;
+
+    private void Start()
+    {
+        playerInRange = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("interact"))
+        {
+            if (playerInRange)
+            {
+                MoveCharacter();
+                forceTrigger = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+            visualCue.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            visualCue.SetActive(false);
+            playerInRange = false;
+        }
+    }
+
+    void MoveCharacter()
+    {
+        if (locationTrigger)
+        {
+            player.transform.position = location;
+        }
+        else if (sceneTrigger)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+    
+}
