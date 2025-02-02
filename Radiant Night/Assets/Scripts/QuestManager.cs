2afc17ec5188dde20d618dd237d6b1d86b38035c
CJPobol <cjp25@njit.edu>
2025-02-02 11:51:03 -0500

    new quest popup with fade out effect

--- Radiant Night/Assets/Scripts/QuestManager.cs
+++ Radiant Night/Assets/Scripts/QuestManager.cs
@@ -11,6 +11,7 @@ public class QuestManager : MonoBehaviour
     public TextMeshProUGUI descriptionText, rewardText;
     public Transform questContainer;
     public GameObject questItemPrefab;
+    public GameObject popup;
 
     public GameObject questSystem;
 
@@ -25,36 +26,37 @@ public class QuestManager : MonoBehaviour
             Debug.LogWarning("More than one Quest Manager.");
         }
         instance = this;
+        questSystem.SetActive(false);
 
-        quests.Add(new Quest
+        AddQuest(new Quest
         {
             questName = "Meeting the Brothers",
             questDescription = "A stranger calling himself “Charlie” has offered to bring you back to his house. You’ve always been told to be wary of people you’ve never met, but where else could you even go?",
             questReward = "Test Reward List 1",
             questCategory = Category.Tutorial
         });
-        quests.Add(new Quest
+        AddQuest(new Quest
         {
             questName = "Search the Forest",
             questDescription = "With odd machine-like creatures running around on Earth, you feel as though it is your duty to investigate and protect the Earthlings. To do so, you’ll need to find more of them in the forest you landed in.",
             questReward = "Test Reward List 2",
             questCategory = Category.Story
         });
-        quests.Add(new Quest
+        AddQuest(new Quest
         {
             questName = "College Day",
             questDescription = "Jacke has offered to take you along with them to college for a day. A bit of socialization might do you some good, maybe you should take them up on it! ",
             questReward = "Test Reward List 3",
             questCategory = Category.Character
         });
-        quests.Add(new Quest
+        AddQuest(new Quest
         {
             questName = "Robot Extermination",
-            questDescription = "Test Description 4",
+            questDescription = "A local shopkeeper has requested that you defend his ranch from the mosnters roaming Earth. He offered a hefty reward for each time you fend off the beasts.",
             questReward = "Test Reward List 4",
             questCategory = Category.World
         });
-        PopulateQuestList();
+        
     }
 
     private void Update()
@@ -68,22 +70,28 @@ public class QuestManager : MonoBehaviour
         return instance;
     }
 
-    public void PopulateQuestList()
-    {
-        foreach (Quest quest in quests)
-        {
-            GameObject questItem = Instantiate(questItemPrefab, questContainer);
-            questItem.GetComponentInChildren<TextMeshProUGUI>().text = quest.questName;
-            questItem.GetComponent<QuestTypeDisplay>().category = quest.questCategory;
-            questItem.GetComponent<Button>().onClick.AddListener(() => ShowQuestDetails(quest));
-        }
-    }
+    
 
     public void AddQuest(Quest quest)
     {
-        //TODO: pop up telling the player a new quest has been added to their journal
         quests.Add(quest);
-        PopulateQuestList();
+        GameObject questItem = Instantiate(questItemPrefab, questContainer);
+        questItem.GetComponentInChildren<TextMeshProUGUI>().text = quest.questName;
+        questItem.GetComponent<QuestTypeDisplay>().category = quest.questCategory;
+        questItem.GetComponent<Button>().onClick.AddListener(() => ShowQuestDetails(quest));
+        StartCoroutine(MakePopup());
+    }
+
+    public IEnumerator MakePopup()
+    {
+        popup.SetActive(true);
+        yield return new WaitForSeconds(3);
+        for (float f = 1; f > -0.04f; f -= 0.04f)
+        {
+            popup.GetComponent<CanvasGroup>().alpha = f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        popup.SetActive(false);
     }
 
     public void ShowQuestDetails(Quest quest)
